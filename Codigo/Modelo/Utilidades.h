1d907067403e77f6eff5b04c37527267a6b64d00
Uriel Alejandro <uaandrade@espe.edu.ec>
2025-05-21 18:22:36 -0500

    Create Utilidades.h
    Se agrega la clase Utilidades.h

---
+++ Codigo/Modelo/Utilidades.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef UTILIDADES_H
+#define UTILIDADES_H
+
+#include <string>
+
+class Utilidades {
+public:
+    // Constructor y destructor
+    Utilidades();
+    ~Utilidades();
+
+    // Métodos para conversiones
+    static double ConvertirADouble(const std::string& texto);
+
+    // Métodos para formateo
+    static std::string FormatearMonto(double monto, int decimales = 2);
+    static std::string FormatearFecha(int dia, int mes, int anio);
+
+    // Validaciones
+    static bool EsNumerico(const std::string& texto);
+    static bool EsCorreoValido(const std::string& correo);
+
+    // Operaciones con strings
+    static std::string EliminarEspacios(const std::string& texto);
+    static std::string ConvertirAMayusculas(const std::string& texto);
+    static std::string ConvertirAMinusculas(const std::string& texto);
+
+    // Metodo para regresar al menu principal
+    static std::string Regresar(); 
+
+};
+
+#endif // UTILIDADES_H
